e5dfb10f544442df7091301f348f6beaf40196d2
Danish <53439438+danish-dotcom@users.noreply.github.com>
2020-06-24 13:34:49 -0400

    Seasons App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { getByDisplayValue } from '@testing-library/react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            err: "",
+            lat: null,
+            month: null
+        }
+    }
+
+    componentDidMount() {
+        // get latitude
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            error => {
+                this.setState({ err: error.message })
+            }
+        )
+
+        // get month
+        this.setState({ month: (new Date()).getMonth() });
+    }
+
+    getDisplay() {
+        if (!this.state.lat && this.state.err) {
+            return (<p className="px-2 pt-4 text-danger">Error: {this.state.err}</p>)
+        } else if (!this.state.month) {
+            return (<p className="px-2 pt-4 text-danger">Error: Month Cannot be Determined</p>)
+        } else{
+            return (<p className="px-2 pt-4 text-success">{this.getSeason()}</p>) 
+        }
+    }
+
+    getSeason() {
+        if (this.state.lat > 0) {
+            if (this.state.month > 9 && this.state.month <2) {
+                return "Winter"
+            } else {
+                return "Summer"
+            }
+        } else if (this.state.lat < 0) {
+            if (this.state.month > 2 && this.state.month <9) {
+                return "Winter"
+            } else {
+                return "Summer"
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="mx-3">
+                <h1 className="display-1">Seasons</h1>
+                {this.getDisplay()}
+            </div>
+        )
+    }
 }
-
-export default App;
