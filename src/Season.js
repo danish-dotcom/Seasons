b4860b613ed3c84fa75fa509327094783adef991
Danish <53439438+danish-dotcom@users.noreply.github.com>
2020-06-24 21:58:07 -0400

    cleaned up unnecessary code

--- src/Season.js
+++ src/Season.js
@@ -13,11 +13,10 @@ const seasonConfig = {
 
 const Season = (props) => {
     const { text, iconName } = seasonConfig[props.season];
-    // console.log (props.season)
 
     return (
         <div>
-            <i className={`icon-left massive ${iconName} icon`}></i>
+            <i className={`icon massive ${iconName} icon`}></i>
             <h5 className="px-2 pt-4"> {text} </h5>
         </div>
     )
